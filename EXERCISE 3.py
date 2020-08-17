a6862ddbd5216cdc5593e689aca28e0468f42aea
fernando-stack <69805393+fernando-stack@users.noreply.github.com>
2020-08-17 11:37:03 -0400

    Add files via upload

---
+++ EXERCISE 3.py
@@ -0,0 +1,117 @@
+import subprocess
+import psutil
+
+print("** WHAT WOULD YOU LIKE TO DO? **")
+print("Option 1: See windows IP configuration")
+print("Option 2: See the system information")
+print("Option 3: ping to www.google.com")
+print("Option 4: See CPU Statistics")
+print("Option 5: See SWAP Memory information")
+
+option = int(input("SELECT AN OPTION: "))
+
+
+if option == 1:
+    subprocess.run(["ipconfig"])
+    more = str(input("WOULD YOU LIKE TO SELECT ANOTHER OPTION? (Y/N): "))
+    while more == "Y":
+        option = int(input("SELECT AN OPTION: "))
+        if option == 1:
+            subprocess.run(["ipconfig"])
+        elif option == 2:
+            subprocess.run(["msinfo32"])
+            print("System information window is opened")
+        elif option == 3:
+            subprocess.run(["ping", "www.google.com"])
+        elif option == 4:
+            print(psutil.cpu_stats())
+        elif option == 5:
+            print(psutil.swap_memory())
+        if option > 5 or option <= 0:
+            print("Please select from options list")
+    else:
+        print("End of Program, see you!")
+
+elif option == 2:
+    subprocess.run(["msinfo32"])
+    print("System information window is opened")
+    more = str(input("WOULD YOU LIKE TO SELECT ANOTHER OPTION? (Y/N): "))
+    while more == "Y":
+        option = int(input("SELECT AN OPTION: "))
+        if option == 1:
+            subprocess.run(["ipconfig"])
+        elif option == 2:
+            subprocess.run(["msinfo32"])
+            print("System information window is opened")
+        elif option == 3:
+            subprocess.run(["ping", "www.google.com"])
+        elif option == 4:
+            print(psutil.cpu_stats())
+        elif option == 5:
+            print(psutil.swap_memory())
+        if option > 5 or option <= 0:
+            print("Please select from options list")
+    else:
+        print("End of Program, see you!")
+elif option == 3:
+    subprocess.run(["ping", "www.google.com"])
+    more = str(input("WOULD YOU LIKE TO SELECT ANOTHER OPTION? (Y/N): "))
+    while more == "Y":
+        option = int(input("SELECT AN OPTION: "))
+        if option == 1:
+            subprocess.run(["ipconfig"])
+        elif option == 2:
+            subprocess.run(["msinfo32"])
+            print("System information window is opened")
+        elif option == 3:
+            subprocess.run(["ping", "www.google.com"])
+        elif option == 4:
+            print(psutil.cpu_stats())
+        elif option == 5:
+            print(psutil.swap_memory())
+        if option > 5 or option <= 0:
+            print("Please select from options list")
+    else:
+        print("End of Program, see you!")
+elif option == 4:
+    print(psutil.cpu_stats())
+    more = str(input("WOULD YOU LIKE TO SELECT ANOTHER OPTION? (Y/N): "))
+    while more == "Y":
+        option = int(input("SELECT AN OPTION: "))
+        if option == 1:
+            subprocess.run(["ipconfig"])
+        elif option == 2:
+            subprocess.run(["msinfo32"])
+            print("System information window is opened")
+        elif option == 3:
+            subprocess.run(["ping", "www.google.com"])
+        elif option == 4:
+            print(psutil.cpu_stats())
+        elif option == 5:
+            print(psutil.swap_memory())
+        if option > 5 or option <= 0:
+            print("Please select from options list")
+    else:
+        print("End of Program, see you!")
+elif option == 5:
+    print(psutil.swap_memory())
+    more = str(input("WOULD YOU LIKE TO SELECT ANOTHER OPTION? (Y/N): "))
+    while more == "Y":
+        option = int(input("SELECT AN OPTION: "))
+        if option == 1:
+            subprocess.run(["ipconfig"])
+        elif option == 2:
+            subprocess.run(["msinfo32"])
+            print("System information window is opened")
+        elif option == 3:
+            subprocess.run(["ping", "www.google.com"])
+        elif option == 4:
+            print(psutil.cpu_stats())
+        elif option == 5:
+            print(psutil.swap_memory())
+        if option > 5 or option <= 0:
+            print("Please select from options list")
+    else:
+        print("End of Program, see you!")
+else:
+    print("You have not selected an option from the list")
